1977b93fcf0e6f2485d76a354bdd4d0465488391
Anthony Vallee <tony.vallee@hotmail.com>
2017-10-10 01:40:12 -0400

    Adds user confirmation before execution, cleanup

--- rap/watermark/entry.py
+++ rap/watermark/entry.py
@@ -1,19 +1,15 @@
-import concurrent.futures
-import functools
 import os
 import shutil
-import timeit
 import threading
 
 import click
-from PIL import Image
 
 from rap.common.utils import filesystem as fs
-from rap.common.utils import timeutils
+from rap.common.utils import timeutils as t
 from rap.common.utils import parallel as p
+
 from rap.watermark import config_setup as c
 from rap.watermark import watermark as w
-from rap.watermark.defaults import config as default_config
 
 HELP = {
     "edit":    "Edit the configuration file.",
@@ -46,6 +42,7 @@ LOG_LOCK = threading.RLock()
 
 @click.group()
 def cli():
+    print("Welcome to watermark...")
     c.init([CONFIG_SRC, WATERMARK_SRC, FONT_SRC], [PACKAGE_CONFIG_SRC, PACKAGE_WATERMARK_SRC, PACKAGE_FONT_SRC])
     pass
 
@@ -68,8 +65,9 @@ def reset():
 @click.argument("directory", type=click.Path(exists=True))
 @click.option("-v", "--verbose", is_flag=True, help=HELP["verbose"])
 def run(directory, config=None, edit_config=None, verbose=None):
-    print("Welcome to watermark...")
-    timespan, _ = timeutils.timeit(run_task, directory, verbose)
+    print("This utility might be uninterruptible once started.")
+    click.confirm("Apply a watermark to all images in current folder and all subfolders ?", abort=True)
+    timespan, _ = t.timeit(run_task, directory, verbose)
     print("Done. Execution took {0:.2f} seconds.".format(timespan))
 
 
